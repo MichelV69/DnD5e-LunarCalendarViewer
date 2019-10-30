9597be1f72cbc2e42e7f889725f8d81d376a1a79
Michel R Vaillancourt <michel.vaillancourt@thinkingbig.net>
2019-10-30 10:23:11 -0300

    Calendar Config via Properties App will now read calendar configuration from a standard JAVA-format 'properties' file.

--- src/main/java/com/pe902_gaming/CLILunarCalendar/App.java
+++ src/main/java/com/pe902_gaming/CLILunarCalendar/App.java
@@ -8,10 +8,16 @@ public class App
 {
   public static void main( String[] args )
   {
-    ReadCalendarProperties CalPropFile = new ReadCalendarProperties()
+    ReadCalendarProperties CalPropFile = new ReadCalendarProperties();
     if (args.length > 0 ) 
     {
        CalPropFile.setPropertiesFileName(args[0]);
+       System.out.println("\n----\nLooking to load Calendar configuration from [" + CalPropFile.getPropertiesFileName() + "]" );
+       if (CalPropFile.FileExists()) 
+       {
+          CalPropFile.LoadFromFile();
+          System.out.println("... should be loaded now ... ");
+       }
     }
 
     // --- initial vars and objects
@@ -20,78 +26,100 @@ public class App
     LunarCalendar OurCalendar = new LunarCalendar();
 
     // --- Start getting data for the Calendar
-    System.out.println("\n----\nCalendar Geometry");
-    System.out.println("CalendarYearInDays?");
-    UserTextInput = ReadUserInput.nextLine();
-    OurCalendar.setCalendarYearInDays(Double.parseDouble(UserTextInput));
+    //CalPropFile.CalProp.getProperty("CalendarYearInDays")
 
-    System.out.println("YearZero?");
-    UserTextInput = ReadUserInput.nextLine();
-    OurCalendar.setYearZero(Integer.parseInt(UserTextInput));
-
-    System.out.println("DayZero?");
-    UserTextInput = ReadUserInput.nextLine();
-    OurCalendar.setDayZero(Integer.parseInt(UserTextInput));
-
-    System.out.println("\n----\nOrbital Periods");
-    System.out.println("Object1Name?");
-    UserTextInput = ReadUserInput.nextLine();
-    if (UserTextInput.isEmpty()) 
+    if ((CalPropFile.getFileDidLoadOK()) && (Double.parseDouble(CalPropFile.CalProp.getProperty("CalendarYearInDays")) > 0)) 
     {
-      UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
+      // we got calendar config from file load it up
+      OurCalendar.setCalendarYearInDays(Double.parseDouble(CalPropFile.CalProp.getProperty("CalendarYearInDays")));
+      OurCalendar.setYearZero(Integer.parseInt(CalPropFile.CalProp.getProperty("YearZero")));
+      OurCalendar.setDayZero(Integer.parseInt(CalPropFile.CalProp.getProperty("DayZero")));
+
+      OurCalendar.setObject1Name(CalPropFile.CalProp.getProperty("Object1Name"));
+      OurCalendar.setObject1PeriodInDays(Double.parseDouble(CalPropFile.CalProp.getProperty("Object1PeriodInDays")));
+      OurCalendar.setObject2Name(CalPropFile.CalProp.getProperty("Object2Name"));
+      OurCalendar.setObject2PeriodInDays(Double.parseDouble(CalPropFile.CalProp.getProperty("Object2PeriodInDays")));
+      OurCalendar.setObject3Name(CalPropFile.CalProp.getProperty("Object3Name"));
+      OurCalendar.setObject3PeriodInDays(Double.parseDouble(CalPropFile.CalProp.getProperty("Object3PeriodInDays")));
+
     }
-    OurCalendar.setObject1Name(UserTextInput);
-
-    if (OurCalendar.getObject1Name() != OurCalendar.ORBIT_IS_EMPTY) 
+    else
     {
-      System.out.println("Object1PeriodInDays?");
+      // get the info interactive from the user
+
+      System.out.println("\n----\nCalendar Geometry");
+      System.out.println("CalendarYearInDays?");
+      UserTextInput = ReadUserInput.nextLine();
+      OurCalendar.setCalendarYearInDays(Double.parseDouble(UserTextInput));
+
+      System.out.println("YearZero?");
+      UserTextInput = ReadUserInput.nextLine();
+      OurCalendar.setYearZero(Integer.parseInt(UserTextInput));
+
+      System.out.println("DayZero?");
+      UserTextInput = ReadUserInput.nextLine();
+      OurCalendar.setDayZero(Integer.parseInt(UserTextInput));
+
+      System.out.println("\n----\nOrbital Periods");
+      System.out.println("Object1Name?");
       UserTextInput = ReadUserInput.nextLine();
       if (UserTextInput.isEmpty()) 
       {
-        UserTextInput = "0.0";
-      }        
-      OurCalendar.setObject1PeriodInDays(Double.parseDouble(UserTextInput));      
-    }
-
-
-    System.out.println("Object2Name?");
-    UserTextInput = ReadUserInput.nextLine();
-    if (UserTextInput.isEmpty()) 
-    {
-      UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
-    }
-    OurCalendar.setObject2Name(UserTextInput);
-
-    if (OurCalendar.getObject2Name() != OurCalendar.ORBIT_IS_EMPTY) 
-    {
-      System.out.println("Object2PeriodInDays?");
-      UserTextInput = ReadUserInput.nextLine();
-      if (UserTextInput.isEmpty()) 
-      {
-        UserTextInput = "0.0";
-      }   
-      OurCalendar.setObject2PeriodInDays(Double.parseDouble(UserTextInput));      
-    }
-
-    System.out.println("Object3Name?");
-    UserTextInput = ReadUserInput.nextLine();
-    if (UserTextInput.isEmpty()) 
-    {
-      UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
-    }
-    OurCalendar.setObject3Name(UserTextInput);
-
-    if (OurCalendar.getObject3Name() != OurCalendar.ORBIT_IS_EMPTY) 
-    {
-      System.out.println("Object3PeriodInDays?");
-      UserTextInput = ReadUserInput.nextLine();
-      if (UserTextInput.isEmpty()) 
-      {
-        UserTextInput = "0.0";
+        UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
       }
-      OurCalendar.setObject3PeriodInDays(Double.parseDouble(UserTextInput));
+      OurCalendar.setObject1Name(UserTextInput);
+
+      if (OurCalendar.getObject1Name() != OurCalendar.ORBIT_IS_EMPTY) 
+      {
+        System.out.println("Object1PeriodInDays?");
+        UserTextInput = ReadUserInput.nextLine();
+        if (UserTextInput.isEmpty()) 
+        {
+          UserTextInput = "0.0";
+        }        
+        OurCalendar.setObject1PeriodInDays(Double.parseDouble(UserTextInput));      
+      }
+
+      System.out.println("Object2Name?");
+      UserTextInput = ReadUserInput.nextLine();
+      if (UserTextInput.isEmpty()) 
+      {
+        UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
+      }
+      OurCalendar.setObject2Name(UserTextInput);
+
+      if (OurCalendar.getObject2Name() != OurCalendar.ORBIT_IS_EMPTY) 
+      {
+        System.out.println("Object2PeriodInDays?");
+        UserTextInput = ReadUserInput.nextLine();
+        if (UserTextInput.isEmpty()) 
+        {
+          UserTextInput = "0.0";
+        }   
+        OurCalendar.setObject2PeriodInDays(Double.parseDouble(UserTextInput));      
+      }
+
+      System.out.println("Object3Name?");
+      UserTextInput = ReadUserInput.nextLine();
+      if (UserTextInput.isEmpty()) 
+      {
+        UserTextInput = OurCalendar.ORBIT_IS_EMPTY;
+      }
+      OurCalendar.setObject3Name(UserTextInput);
+
+      if (OurCalendar.getObject3Name() != OurCalendar.ORBIT_IS_EMPTY) 
+      {
+        System.out.println("Object3PeriodInDays?");
+        UserTextInput = ReadUserInput.nextLine();
+        if (UserTextInput.isEmpty()) 
+        {
+          UserTextInput = "0.0";
+        }
+        OurCalendar.setObject3PeriodInDays(Double.parseDouble(UserTextInput));
+      }
     }
 
+    // now interactive-get initial observation data
     Boolean DoAgain = true;
     Boolean CalendarSlide = false;
     int CalendarSlideStepDays = 0;
--- src/main/java/com/pe902_gaming/CLILunarCalendar/ReadCalendarProperties.java
+++ src/main/java/com/pe902_gaming/CLILunarCalendar/ReadCalendarProperties.java
@@ -1,6 +1,7 @@
 package com.pe902_gaming.CLILunarCalendar;
 
 import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,18 +10,20 @@ public class ReadCalendarProperties
 {
   // properties
   private String PropertiesFileName = "";
+  public  Properties CalProp = new Properties();
+  private Boolean FileDidLoadOK = false;
 
     // constructor
-  public ReadCalendarProperties(String propertiesFileName)
+  public ReadCalendarProperties()
   {
-    PropertiesFileName = propertiesFileName;
-    System.out.println("Will load calendar configuration from [" + PropertiesFileName + "]" );
+    // PropertiesFileName = propertiesFileName;
+    // System.out.println("Will load calendar configuration from [" + PropertiesFileName + "]" );
   } // end method ReadCalendarProperties
 
   // other methods 
   public void setPropertiesFileName (String newFileName)
   {
-    if (!newFileName.isEmtpy()) 
+    if (!newFileName.isEmpty()) 
     {
       PropertiesFileName = newFileName;
     }
@@ -29,6 +32,30 @@ public class ReadCalendarProperties
   public String getPropertiesFileName ()
   {
     return PropertiesFileName;
+  } // end getPropertiesFileName
+
+  public boolean getFileDidLoadOK ()
+  {
+    return FileDidLoadOK;
   }
+
+  public Boolean FileExists()
+  {
+    return new File(PropertiesFileName).isFile();
+  } // end FileExists
+
+  public void LoadFromFile()
+  {
+    try (FileInputStream filePointer = new FileInputStream(PropertiesFileName))
+    {
+      CalProp.load(filePointer);
+      filePointer.close();
+      FileDidLoadOK = true;
+    }
+    catch (IOException ex) 
+    {
+      ex.printStackTrace();
+    }
+  } // end LoadFromFile
 
 } //end class ReadCalendarProperties
